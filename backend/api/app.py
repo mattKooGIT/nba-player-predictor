89f2d455c9ef2a1a66019bd22e72ecfb24db312f
mattKooGIT <matthewkoo366@gmail.com>
2025-06-18 23:49:34 -0700

    awefawef

--- backend/api/app.py
+++ backend/api/app.py
@@ -33,4 +33,5 @@ def predict():
 
 if __name__ == "__main__":
     # Use 0.0.0.0 so it listens externally on Render
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
